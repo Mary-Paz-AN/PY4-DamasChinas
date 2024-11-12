56d5d84e1d6cb47c7edd11ee4e9f6cb0f5ac74af
Estefani Valverde <88058530+Estefani05@users.noreply.github.com>
2024-11-12 17:13:36 -0600

    Add files via upload

--- codigo/frontend/src/index.js
+++ codigo/frontend/src/index.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; 
 
-//Creaters the root container
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-//Creates as path for the pages to navegate in them
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
